17adb644d72688a6f5c46f8f7d1e62cd0e2d84f7
madison 'maddy' roche <madisonroche01@gmail.com>
2026-04-17 12:02:49 +0100

    comments added cookies.php

--- website/part_five/cookies.php
+++ website/part_five/cookies.php
@@ -7,16 +7,23 @@
 
 if (!isset($_COOKIE['username'])) {
 
+    // creates cookie and assigns the cookie the username maddy to reprint later 
     setcookie("username", "maddy", time() + 3600);
+    // instructs user to refresh the page to view the retained cookie information
     echo "Cookie created! Refresh the page.";
 
 } else {
 
+    // reprints the username assigned to the cookie to the web application
     echo "Cookie exists: " . $_COOKIE['username'];
-
+    // deletes the cookie assigned to the username 
     setcookie("username", "", time() - 3600);
     echo "<br>Cookie deleted!";
 }
+
+    // next time you refresh the page a new cookie is made
+    // refresh again and it is displayed with the user + deleted again
+    // done this way for quick demo / easy screenshots rather than waiting for ages
 ?>
 
 </body>
