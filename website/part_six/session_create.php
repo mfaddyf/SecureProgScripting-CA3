a929a24fb037f62fa0092f90419797d173f9dedd
madison 'maddy' roche <madisonroche01@gmail.com>
2026-04-18 20:12:33 +0100

    comment and reference (for all) session_create.php

--- website/part_six/session_create.php
+++ website/part_six/session_create.php
@@ -9,4 +9,9 @@ echo "Username stored in session<br>";
 
 // Show session ID
 echo "Session ID: " . session_id();
+
+  // ----- references FOR ALL THREE PARTS!!!
+  // https://www.w3schools.com/php/php_sessions.asp
+  // https://www.php.net/manual/en/book.session.php
+  // https://www.geeksforgeeks.org/php/php-sessions/
 ?>
